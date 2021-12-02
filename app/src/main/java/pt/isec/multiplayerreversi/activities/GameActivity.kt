5ca54e5e4b6fb0cdcf3f25470183f4bbe27ec346
Zigko <cenas.dorin2@hotmail.com>
2021-12-02 21:51:27 +0000

    looks like sh*** and need to be tested with 3 players

--- app/src/main/java/pt/isec/multiplayerreversi/activities/GameActivity.kt
+++ app/src/main/java/pt/isec/multiplayerreversi/activities/GameActivity.kt
@@ -1,12 +1,16 @@
 package pt.isec.multiplayerreversi.activities
 
 import android.app.AlertDialog
+import android.app.Dialog
 import android.graphics.Color
 import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.get
@@ -16,6 +20,7 @@ import pt.isec.multiplayerreversi.R
 import pt.isec.multiplayerreversi.databinding.ActivityGameBinding
 import pt.isec.multiplayerreversi.game.GameGrid
 import pt.isec.multiplayerreversi.game.interactors.GamePlayer
+import pt.isec.multiplayerreversi.game.logic.GameEndStats
 import pt.isec.multiplayerreversi.game.logic.Vector
 import kotlin.concurrent.thread
 
@@ -94,8 +99,12 @@ class GameActivity : AppCompatActivity() {
             }
         }
 
-        binding.btnPass.setOnClickListener {
-            proxy.passPlayer()
+        binding.btnPass.setOnClickListener{
+            if (proxy.getPossibleMoves().isEmpty()){
+                it.visibility = View.VISIBLE
+                proxy.passPlayer()
+            }else
+                it.visibility = View.INVISIBLE
         }
     }
 
@@ -136,7 +145,8 @@ class GameActivity : AppCompatActivity() {
 
         proxy.gameFinishedCallback = {
             runOnUiThread {
-                Toast.makeText(this, "Game finished", Toast.LENGTH_SHORT).show()
+                //Toast.makeText(this, "Game finished", Toast.LENGTH_SHORT).show()
+                openEndGameLayoutDialog(it)
                 val playerStats =
                     it.playerStats.find { p -> p.player.playerId == it.winningPlayerId }
                 if (playerStats != null) {
@@ -156,6 +166,45 @@ class GameActivity : AppCompatActivity() {
 
     //TODO SET INVISIBLE BUTTON
     //TODO winner pop up on end game
+
+    private fun openEndGameLayoutDialog(gameEndStats: GameEndStats){
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.end_game_dialog_layout)
+        val tvWinner = dialog.findViewById(R.id.tvWinner) as TextView
+        val tvWinnerScore = dialog.findViewById(R.id.tvWinnerScore) as TextView
+        val tvOpponent1 = dialog.findViewById(R.id.tvOpponent1) as TextView
+        val tvScoreOpponent1 = dialog.findViewById(R.id.tvScoreOpponent1) as TextView
+        val tvOpponent2 = dialog.findViewById(R.id.tvOpponent2) as TextView
+        val tvScoreOpponent2 = dialog.findViewById(R.id.tvScoreOpponent2) as TextView
+        val btnOk = dialog.findViewById(R.id.btnOk) as Button
+
+        gameEndStats.playerStats.forEach {
+            if (it.player.playerId == gameEndStats.winningPlayerId){
+                tvWinner.text = it.player.profile.name
+                tvWinnerScore.text = it.pieces.toString()
+            }else{
+                tvOpponent1.text = it.player.profile.name
+                tvScoreOpponent1.text = it.pieces.toString()
+                if (gameEndStats.playerStats.size > 2){
+                    val flThirdPlayer = dialog.findViewById(R.id.lnThirdPlayer) as View
+                    flThirdPlayer.visibility = View.VISIBLE
+                    if (tvOpponent1.text != it.player.profile.name ){
+                        tvOpponent2.text = it.player.profile.name
+                        tvScoreOpponent2.text = it.pieces.toString()
+                        //TODO test isto
+                    }
+                }
+            }
+        }
+        btnOk.setOnClickListener{
+            dialog.dismiss()
+            finish()
+        }
+
+        dialog.show()
+    }
 
     override fun onBackPressed() {
         val alertDialog = AlertDialog.Builder(this)

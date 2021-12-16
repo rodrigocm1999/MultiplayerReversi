c038ba39275de6b3050a9e9c22041f89e05892de
Zigko <cenas.dorin2@hotmail.com>
2021-12-16 17:23:27 +0000

    stufed code more

--- app/src/main/java/pt/isec/multiplayerreversi/activities/others/FirestoreHelper.kt
+++ app/src/main/java/pt/isec/multiplayerreversi/activities/others/FirestoreHelper.kt
@@ -17,24 +17,23 @@ class FirestoreHelper(private val id: String) {
 
     fun insertData(gameEndStats: GameEndStats, playerScore: Int) {
         val playerStats = gameEndStats.playerStats
-        val players: ArrayList<HistoryActivity.Player> = ArrayList(3)
+        val players: ArrayList<Player> = ArrayList(3)
         var topSocore = 0
         playerStats.forEach {
-            val player: HistoryActivity.Player =
-                HistoryActivity.Player(it.player.profile.name, it.pieces)
+            val player = Player(it.player.profile.name, it.pieces)
             if (it.player.playerId == gameEndStats.winningPlayerId)
                 topSocore = it.pieces
             players.add(player)
         }
 
         //TODO addcionar as imagems dos players
-        val game = HistoryActivity.Game(players, playerScore)
+        val game = Game(players, playerScore)
         val updatedGames = updateTopScores(game)
         val scores = hashMapOf("games" to updatedGames, "topscore" to topSocore)
         getPlayerDocument().set(scores)
     }
 
-    private fun updateTopScores(newGame: HistoryActivity.Game): MutableList<HistoryActivity.Game> {
+    private fun updateTopScores(newGame: Game): MutableList<Game> {
         val gamesHistory = getGamesHistory()
         gamesHistory.add(newGame)
         val sortedByDescending =
@@ -48,10 +47,21 @@ class FirestoreHelper(private val id: String) {
         return getPlayerDocument().get().result.get("topscore") as Int
     }
 
-    fun getGamesHistory(): ArrayList<HistoryActivity.Game> {
+    fun getGamesHistory(): ArrayList<Game> {
         val document = Tasks.await(getPlayerDocument().get())
-        return if (document != null) document.get("games") as ArrayList<HistoryActivity.Game>
+        val result =  document.get("games")
+        return if (result != null) result as ArrayList<Game>
         else ArrayList(5)
     }
+    class Player(
+        var name : String,
+        var score : Int
+    ){
+
+    }
+    class Game(
+        var players: ArrayList<Player>? = null,
+        var playerScore : Int
+    )
 
 }

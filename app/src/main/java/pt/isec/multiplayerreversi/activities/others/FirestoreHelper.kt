f771bcce198f113905a5ecfc514dad346929441f
rodrigo mendes <rodrigocm1999@hotmail.com>
2021-12-16 17:42:17 +0000

    .

--- app/src/main/java/pt/isec/multiplayerreversi/activities/others/FirestoreHelper.kt
+++ app/src/main/java/pt/isec/multiplayerreversi/activities/others/FirestoreHelper.kt
@@ -3,8 +3,8 @@ package pt.isec.multiplayerreversi.activities.others
 import com.google.android.gms.tasks.Tasks
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
-import pt.isec.multiplayerreversi.activities.HistoryActivity
 import pt.isec.multiplayerreversi.game.logic.GameEndStats
 
 class FirestoreHelper(private val id: String) {
@@ -18,26 +18,27 @@ class FirestoreHelper(private val id: String) {
     fun insertData(gameEndStats: GameEndStats, playerScore: Int) {
         val playerStats = gameEndStats.playerStats
         val players: ArrayList<Player> = ArrayList(3)
-        var topSocore = 0
+        var topScore = 0
         playerStats.forEach {
             val player = Player(it.player.profile.name, it.pieces)
             if (it.player.playerId == gameEndStats.winningPlayerId)
-                topSocore = it.pieces
+                topScore = it.pieces
             players.add(player)
         }
 
         //TODO addcionar as imagems dos players
         val game = Game(players, playerScore)
         val updatedGames = updateTopScores(game)
-        val scores = hashMapOf("games" to updatedGames, "topscore" to topSocore)
+        val scores = hashMapOf("games" to updatedGames, "topscore" to topScore)
         getPlayerDocument().set(scores)
     }
 
     private fun updateTopScores(newGame: Game): MutableList<Game> {
         val gamesHistory = getGamesHistory()
-        gamesHistory.add(newGame)
+        val games = gamesHistory.games
+        games.add(newGame)
         val sortedByDescending =
-            gamesHistory.sortedByDescending { it.playerScore }.toMutableList()
+            games.sortedByDescending { it.playerScore }.toMutableList()
         if (sortedByDescending.size > 5) sortedByDescending.removeLast()
         return sortedByDescending
     }
@@ -47,21 +48,26 @@ class FirestoreHelper(private val id: String) {
         return getPlayerDocument().get().result.get("topscore") as Int
     }
 
-    fun getGamesHistory(): ArrayList<Game> {
+    fun getGamesHistory(): History {
         val document = Tasks.await(getPlayerDocument().get())
-        val result =  document.get("games")
-        return if (result != null) result as ArrayList<Game>
-        else ArrayList(5)
+        val result = document.toObject<History?>()
+        if (result != null) return result
+        return History()
     }
-    class Player(
-        var name : String,
-        var score : Int
-    ){
 
-    }
+    class Player(
+        var name: String = "",
+        var score: Int = 0,
+    )
+
     class Game(
-        var players: ArrayList<Player>? = null,
-        var playerScore : Int
+        var players: ArrayList<Player> = ArrayList(0),
+        var playerScore: Int = 0,
+    )
+
+    class History(
+        var games: ArrayList<Game> = ArrayList(0),
+        var topscore: Int = 0,
     )
 
 }
